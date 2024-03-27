c2de063b7f47574a1a08831421b99b022817ff5f
Sascha Lucius <sascha.lucius@datameer.com>
2024-03-27 23:09:17 +0100

    Update index.cjs

--- src/index.cjs
+++ src/index.cjs
@@ -3,39 +3,46 @@ const fs = require('fs');
 const readline = require('readline');
 
 const docsFolder = './docs/';
-const guides = './src/lib/generated/guides.ts'
-const tags = './src/lib/generated/tags.ts'
+const guidesPath = './src/lib/generated/guides.ts'
+const tagsPath = './src/lib/generated/tags.ts'
+
+const REGEX_BRACKETS = /\[[#A-Za-z:,?-]+\]/gi;
 
 let allTags = [];
 
 // Cleanup
-fs.unlinkSync(guides);
-fs.unlinkSync(tags);
+fs.unlinkSync(guidesPath);
+fs.unlinkSync(tagsPath);
 
-fs.readdir(docsFolder, async (err, files) => {
-    files.forEach(async (file) => {
-        if(file.endsWith('.md')){
-                allTags = [];
-                fs.writeFileSync(guides, `export const ${file.slice(0, -3)} = \``);
-                fs.writeFileSync(tags, `export const ${file.slice(0, -3)} = [`);
-                await processLineByLine(docsFolder+file);
-                fs.appendFileSync(tags, `"${allTags.join('","')}"];`);
-                fs.appendFileSync(guides, "`;");
-        }
-    });
-});
+let tags;
+let guides;
+
+tags = fs.openSync(tagsPath, 'a');
+guides = fs.openSync(guidesPath, 'a');
+
+const files = fs.readdirSync(docsFolder)
+
+for (const file of files) {
+    if(file.endsWith('.md')){
+        allTags = [];
+        fs.appendFileSync(guides, `export const ${file.slice(0, -3)} = \``,'utf8');
+        fs.appendFileSync(tags, `export const ${file.slice(0, -3)} = [`,'utf8');
+        processLineByLine(docsFolder+file);
+        fs.appendFileSync(tags, `"${allTags.join('","')}"];`,'utf8');
+        fs.appendFileSync(guides, "`;",'utf8');
+    }
+}
+
+if (tags !== undefined)
+    fs.closeSync(tags);
+if (guides !== undefined)
+    fs.closeSync(guides);
 
 async function processLineByLine(file) {
-	const fileStream = fs.createReadStream(file);
+	const lines = fs.readFileSync(file, 'utf-8')
+    .split('\n')
 
-	const rl = readline.createInterface({
-		input: fileStream,
-		crlfDelay: Infinity
-	});
-	// Note: we use the crlfDelay option to recognize all instances of CR LF
-	// ('\r\n') in input.txt as a single line break.
-
-	for await (const line of rl) {
+	for (const line of lines) {
 		const plain = line.replace(/(\[[#A-Za-z:,?-]+\])/g, '');
 		const customTags = getTags(line);
 		const stems = natural.PorterStemmer.tokenizeAndStem(plain);
@@ -47,17 +54,17 @@ async function processLineByLine(file) {
         allTags = allTags.concat(unique);
 
 		if (unique.length === 0) {
-            fs.appendFileSync(guides, plain + "\n");
+            fs.appendFileSync(guides, plain + "\n",'utf8');
 		} else {
-            fs.appendFileSync(guides, `${plain}[${unique.join(',')}]\n`);
+            fs.appendFileSync(guides, `${plain}[${unique.join(',')}]\n`,'utf8');
 		}
 	}
 }
 
-const regexpBrackets = /\[[#A-Za-z:,?-]+\]/gi;
+
 
 function getTags(line) {
-	let temp = line.match(regexpBrackets);
+	let temp = line.match(REGEX_BRACKETS);
 	if (temp != null) {
 		return temp.flatMap((t) => t.substring(1, t.length - 1).split(','));
 	}
